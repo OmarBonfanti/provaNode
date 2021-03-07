bc311b81a67700188046e6b581c09d0dae118ac0
OmarBonfanti <51740547+OmarBonfanti@users.noreply.github.com>
2021-03-08 00:58:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,62 +1,20 @@
+  
 var http = require("http");
 
 var express = require("express");
-const { send } = require("process");
-var apiS = express();
+var apiServer = express();
 
-var hostname = "127.0.0.1";
 var port = 3000;
 
-var path = require("path");
-
-var nome = "XD";
-
-/*
-var server = http.createServer((req, res) => {
-    console.log("req:", req);
-    //prepara una res
-    res.statusCode = 200;
-
-    res.setHeader("content-Type", "text/plain");
-    //chiusura respons
-    res.end("Ciao belloccio!");
-});
-*/
-
-apiS.listen(port, hostname, () => {
-    console.log("server running at http://%s:%d/", hostname, port);
+apiServer.listen(port, () => {
+    console.log("server running on port ", port);
 });
 
-//creazione risolsa http//localhost:3000/nome 
-// get = ricevo nome server
-// post = setto nome server
-// put  = 
-// delete =
-
-// api server con express 
-
-apiS.get("/nome",(req,res)=> {
-   console.log("ho ricevuto una get su nome");
-  //  res.statusCode = 200;
-  //  res.setHeader("content-Type", "text/plain");
-    res.end("Check server! il nome è: ",+nome);
+apiServer.get("/nome", (req, res) =>{
+    console.log("ho ricevuto una get su nome");
+    res.send("ciao, il nome del server è: NODEPOGLIANI");
 });
 
-
-
-apiS.get("/",(req,res)=>{
+apiServer.get("/", function(req, res){
     res.send("sei in home");
-
-} );
-
-apiS.get("/index",(req,res)=>{
-    //res.sendFile("C:\\Users\\bonfanti.omar\\Desktop\\provaNode\\index.html");
-    res.sendFile(path.join(__dirname+"/index.html"));
-});
-
-apiS.get("/change_name",(req,res)=>{
-    s
-    res.end("Check server! il nome è: ",+nome);
-});
-
-
+})
